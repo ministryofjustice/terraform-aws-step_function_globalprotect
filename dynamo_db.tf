cfb4d00486e316fcb303eea26271e0e4df58691f
Migara Ekanayake <2110772+migara@users.noreply.github.com>
2021-04-05 12:33:58 +0100

    refactor dynamo db

--- dynamo_db.tf
+++ dynamo_db.tf
@@ -3,19 +3,28 @@ locals {
   newbits = trim(var.gp_pool_subnet_mask, "/") - split("/", var.gp_pool_supernet_cidr_range_ipv4)[1]
 
   # Prep entries for GP gateway table
-  gateway_map = flatten([
-    # for i in range(42) :
-    for i in range(local.newbits) :
-    [for j in range(length(var.availability_zones)) : {
-      Hostname       = format(var.gp_gateway_hostname_template, i + 1, element(var.suffix_map, j))
-      DNSPrefix      = "gw-${lower(element(var.suffix_map, j))}${format("%02d", i + 1)}"
-      AZ             = element(var.availability_zones, j)
-      ClientPoolIPv4 = cidrsubnet(var.gp_pool_supernet_cidr_range_ipv4, local.newbits, (i * length(var.availability_zones)) + j + var.subnets_to_skip)
-      ClientPoolIPv6 = ""
-      # ClientPoolIPv6 = cidrsubnet("2a0d:9b84:100::/56", 8, (i * length(var.availability_zones)) + j + 10)
-      GP_TUNNEL_IP = split("/", cidrsubnet(var.gp_pool_supernet_cidr_range_ipv4, local.newbits, (i * length(var.availability_zones)) + j + var.subnets_to_skip))[0]
-    }]
-  ])
+  # gateway_map = flatten([
+  #   # for i in range(42) :
+  #   for i in range(local.newbits) :
+  #   [for j in range(length(var.availability_zones)) : {
+  #     Hostname       = format(var.gp_gateway_hostname_template, i + 1, element(var.suffix_map, j))
+  #     DNSPrefix      = "gw-${lower(element(var.suffix_map, j))}${format("%02d", i + 1)}"
+  #     AZ             = element(var.availability_zones, j)
+  #     ClientPoolIPv4 = cidrsubnet(var.gp_pool_supernet_cidr_range_ipv4, local.newbits, (i * length(var.availability_zones)) + j + var.subnets_to_skip)
+  #     ClientPoolIPv6 = ""
+  #     # ClientPoolIPv6 = cidrsubnet("2a0d:9b84:100::/56", 8, (i * length(var.availability_zones)) + j + 10)
+  #     GP_TUNNEL_IP = split("/", cidrsubnet(var.gp_pool_supernet_cidr_range_ipv4, local.newbits, (i * length(var.availability_zones)) + j + var.subnets_to_skip))[0]
+  #   }]
+  # ])
+  gateway_map = [for i in pow(2, local.newbits) : {
+    Hostname       = ""
+    DNSPrefix      = ""
+    AZ             = ""
+    ClientPoolIPv4 = cidrsubnet(var.gp_pool_supernet_cidr_range_ipv4, local.newbits, i)
+    ClientPoolIPv6 = ""
+    GP_TUNNEL_IP   = split("/", cidrsubnet(var.gp_pool_supernet_cidr_range_ipv4, local.newbits, i))[0]
+  }]
+
 }
 
 resource "aws_dynamodb_table" "gp" {
@@ -23,11 +32,11 @@ resource "aws_dynamodb_table" "gp" {
   billing_mode   = "PROVISIONED"
   read_capacity  = 5
   write_capacity = 5
-  hash_key       = "Hostname"
+  hash_key       = "ClientPoolIPv4"
 
 
   attribute {
-    name = "Hostname"
+    name = "ClientPoolIPv4"
     type = "S"
   }
 
